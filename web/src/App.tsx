af74c9c84c24c3cf401bf251eb45f2c4432d44f8
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2021-01-29 10:36:59 -0300

    routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello Word!</h1>;
-}
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return <Routes />;
+};
 
 export default App;
