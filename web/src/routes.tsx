c662912bbbdc1a53a1e97bac9bc5110b5ec5798c
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2021-01-31 19:52:38 -0300

    route for pokemon page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
+import PokemonPage from './pages/PokemonPage';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Route path="/" exact component={Landing} />
+    <Route path="/pokemon/:id" component={PokemonPage} />
   </BrowserRouter>
 );
 
