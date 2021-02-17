84e320554482f459a4c3f19939f05c5a8cdd6f42
nilton izaguirres <niltonizaguirres2003@gmail.com>
2021-02-17 18:49:30 -0300

    added fonts and rotes.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+// eslint-disable-next-line camelcase
+import { Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
 
-export default function App() {
+import AppStack from './src/routes/AppStack';
+
+const App: React.FC = () => {
+  const [fontLoaded] = useFonts({ Poppins_700Bold });
+
+  if (!fontLoaded) {
+    return <Text>Não carregou!</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <AppStack />
+      <StatusBar />
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
